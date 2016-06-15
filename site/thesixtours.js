7c49797823d9a9799934b4b72f22885ae585ef18
BleuProfond <p.verteuil@gmail.com>
2016-06-15 11:52:31 -0400

    added some boilerplate code to views and layouts

--- site/thesixtours.js
+++ site/thesixtours.js
@@ -11,28 +11,24 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(req, res){
-  res.type('text/plain');
-  res.send('The Six Tours');
+  res.render('home');
 });
 
 app.get('/about', function(req, res){
-  res.type('text/plain');
-  res.send('About The Six Tours');
+  res.render('about');
 });
 
 // custom 404 page
-app.use(function(req, res){
-  res.type('text/plain');
+app.use(function(req, res, next){
   res.status(404);
-  res.send('404 - Not Found');
+  res.render('404');
 });
 
 // custom 500 page
 app.use(function(err, req, res, next){
   console.error(err.stack);
-  res.type('text/plain');
   res.status(500);
-  res.send('500 - Server Error');
+  res.render('500');
 });
 
 app.listen(app.get('port'), function(){
